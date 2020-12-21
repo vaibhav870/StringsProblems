18225ed3cff348b6cea9639ac19b37ca2071ffa3
Vaibhav M <vaibhav.muddebihalkar@gmail.com>
2020-12-22 01:34:41 +0530

    Update StringJunk.java

--- src/MyString/StringJunk.java
+++ src/MyString/StringJunk.java
@@ -4,7 +4,7 @@ public class StringJunk {
     public static void main(String[] args) {
         String str= "@#@@#@$@#$@%@# vaibhav @#$@%original$%#&%(&*(&$@ string@#$#%#$%$#%# ";
 
-
+        //[a-zA-Z0-9] for all small captial alphaphets and number
         str=str.replaceAll("[^a-zA-Z0-9]","");
         System.out.println(str);
     }
